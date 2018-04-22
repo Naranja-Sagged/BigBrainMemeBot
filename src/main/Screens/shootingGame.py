ce4c9a326d2cc0f0d93632a618848b000ae7130a
Naranja-Sagged <alexliuying@gmail.com>
2018-04-22 05:08:29 -0700

    lel hang
    leled

--- src/main/Screens/shootingGame.py
+++ src/main/Screens/shootingGame.py
@@ -3,6 +3,8 @@ from random import *
 class shootingGame(object):
     def __init__(self):
         super(shootingGame, self).__init__()
+        self.last = pygame.time.get_ticks()
+
 
     def render(self, screen, events, manager):
         self.image = pygame.image.load("Assets/Minigame1/Start.png")
@@ -10,7 +12,24 @@ class shootingGame(object):
         screen.blit(self.image, [0, 0])
         rand_num = randint(5, 10)
         pygame.display.flip()
-        pygame.time.wait(rand_num*1000)
+        beforewait = pygame.time.get_ticks()
+        waiting = True
+        while waiting:
+            now = pygame.time.get_ticks()
+            if now - self.last >= 1:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                        self.image = pygame.image.load("Assets/Minigame1/LeftEarly.png")
+                        self.image = pygame.transform.scale(self.image, (1200, 600))
+                        screen.blit(self.image, [0, 0])
+                        pygame.display.flip()
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_k:
+                        self.image = pygame.image.load("Assets/Minigame1/RightEarly.png")
+                        self.image = pygame.transform.scale(self.image, (1200, 600))
+                        screen.blit(self.image, [0, 0])
+                        pygame.display.flip()
+            if now - beforewait >= rand_num*1000:
+                waiting = False
         self.image = pygame.image.load("Assets/Minigame1/Shoot.png")
         self.image = pygame.transform.scale(self.image, (1200, 600))
         screen.blit(self.image, [0, 0])
