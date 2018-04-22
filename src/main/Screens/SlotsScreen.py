629f738e9164ef19feee425790912873dafa6519
David Febles <dafebles@ucsd.edu>
2018-04-22 05:43:33 -0700

    wow rectangles

--- src/main/Screens/SlotsScreen.py
+++ src/main/Screens/SlotsScreen.py
@@ -12,17 +12,19 @@ class SlotsScreen(object):
         self.image = pygame.image.load("Assets/deathslotz.png")
         self.image = pygame.transform.scale(self.image, (1200, 600))
         screen.blit(self.image, [0, 0])
-
+        pygame.draw.rect(screen, pygame.Color(0, 0, 0), (200, 50, 200, 350), 10)
+        pygame.draw.rect(screen, pygame.Color(0, 0, 0), (500, 50, 200, 350), 10)
+        pygame.draw.rect(screen, pygame.Color(0, 0, 0), (800, 50, 200, 350), 10)
         for x in range(0, 3):
             y = random.randint(0, 2)
-            if (y % 2 == 1):
+            #if (y % 2 == 1):
                 # Display safety image
-                safe = Image("Assets/safety.jpg", (200 + 300 * x, 125), (400 + 300 * x, 325))
-                safe.draw(screen)
-            else:
+                #safe = Image("Assets/safety.jpg", (200 + 300 * x, 125), (400 + 300 * x, 325))
+                #safe.draw(screen)
+            #else:
                 # Display death image
-                death = Image("Assets/safety.jpg", (200 + 300 * x, 125), (400 + 300 * x, 325))
-                death.draw(screen)
+                #death = Image("Assets/safety.jpg", (200 + 300 * x, 125), (400 + 300 * x, 325))
+                #death.draw(screen)
 
     def update(self):
         pass
