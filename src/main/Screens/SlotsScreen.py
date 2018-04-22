3ecc366a967925b5de6a2c491a5b6421eb29d814
MyAngelNeptune <kkc028@ucsd.edu>
2018-04-22 05:26:51 -0700

    fixed more bugs
    fixed bug

--- src/main/Screens/SlotsScreen.py
+++ src/main/Screens/SlotsScreen.py
@@ -1,4 +1,9 @@
 import pygame
+import random
+
+from pygame.locals import *
+from src.main.Screens.graphics import *
+
 class SlotsScreen(object):
     def __init__(self):
         super(SlotsScreen, self).__init__()
@@ -7,6 +12,17 @@ class SlotsScreen(object):
         self.image = pygame.image.load("Assets/deathslotz.png")
         self.image = pygame.transform.scale(self.image, (1200, 600))
         screen.blit(self.image, [0, 0])
+
+        for x in range(0, 3):
+            y = random.randint(0, 2)
+            if (y % 2 == 1):
+                # Display safety image
+                safe = Image("Assets/safety.jpg", (200 + 300 * x, 125), (400 + 300 * x, 325))
+                safe.draw(screen)
+            else:
+                # Display death image
+                death = Image("Assets/safety.jpg", (200 + 300 * x, 125), (400 + 300 * x, 325))
+                death.draw(screen)
 
     def update(self):
         pass
